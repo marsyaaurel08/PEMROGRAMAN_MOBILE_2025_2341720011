38cacf312f8c79c92fa1271c2e92d8c3d38e9f46
=marsyaaurel08 <=sefiramarsya@gmail.com>
2025-09-08 14:13:20 +0700

    Week03_Praktikum1

---
+++ Week03/src/praktikum1.dart
@@ -0,0 +1,17 @@
+void main() {
+String test = "test2";
+if (test == "test1") {
+  print("Test1");
+} else if (test == "test2") {
+  print("Test2");
+} else {
+  print("Something else");
+}
+
+if (test == "test2") print("Test 2 again");
+
+String Test2 = "true";
+if (Test2 == "true") {
+  print("Kebenaran");
+}
+}
