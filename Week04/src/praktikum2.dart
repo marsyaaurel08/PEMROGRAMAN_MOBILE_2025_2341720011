bbe8ec1bb813287a8e74ac5e5b6c1d8c58d90f2a
=marsyaaurel08 <=sefiramarsya@gmail.com>
2025-09-15 15:44:36 +0700

    Week04_Praktikum2

---
+++ Week04/src/praktikum2.dart
@@ -0,0 +1,16 @@
+void main () {
+
+  var halogens = {'flourine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
+
+  var names1 = <String>{};
+  names1.add('MARSYA');
+  Set<String> names2 ={};
+  names2.add('AURELIA');
+  var names3 = <String>{};
+  names3.addAll(['SEFIRA', '2341720011']);
+
+  print(names1);
+  print(names2);
+  print(names3);
+}
