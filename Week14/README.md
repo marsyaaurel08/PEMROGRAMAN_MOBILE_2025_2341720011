1b1d49d1fe4518532ca3d42b25e5eb30e98cf340
marsyaaurel08 <sefiramarsya@gmail.com>
2025-11-30 14:12:49 +0700

    w14_praktikum1_soal1_15

---
+++ Week14/README.md
@@ -0,0 +1,25 @@
+# Pemrograman Mobile Week 14
+
+Nama : Marsya Aurelia Sefira
+
+Kelas : 3G
+
+NIM : 2341720011
+
+# Praktikum 1 : Membuat layanan Mock API
+
+![Mock Api](./store_data_marsya/img/Mock%20API.png)
+
+# Soal 1
+
+Menambah nama panggilan sebagai identitas
+
+![Soal 1 nama panggilan](./store_data_marsya/img/soal1nama.png)
+
+Mengganti warna dengan warna kesukaan
+
+![Soal 1 background](./store_data_marsya/img/soal1bg.png)
+
+Capture hasil praktikum 
+
+![Hasil Praktikum 1](./store_data_marsya/img/soal1.jpg)
---
+++ Week14/store_data_marsya/lib/main.dart
@@ -0,0 +1,176 @@
+import 'dart:convert';
+import '../models/pizza_model.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'httphelper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo Marsya',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String pizzaString = '';
+  List<Pizza> myPizzas = [];
+  int appCounter = 0;
+  String documentsPath = '';
+  String tempPath = '';
+  late File myfile;
+  String filetext = '';
+
+  final pwdController = TextEditingController();
+  String myPass = '';
+  final storage = const FlutterSecureStorage();
+  final myKey = 'myPass';
+
+  Future<List<Pizza>> callPizzas() async {
+    HttpHelper helper = HttpHelper();
+    List<Pizza> pizzas = await helper.getPizzaList();
+    return pizzas;
+  }
+
+  Future<List<Pizza>> readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/pizzalist.json');
+    List pizzaMapList = jsonDecode(myString);
+    List<Pizza> myPizzas = [];
+    for (var pizza in pizzaMapList) {
+      Pizza myPizza = Pizza.fromJson(pizza);
+      myPizzas.add(myPizza);
+    }
+    String json = convertToJSON(myPizzas);
+    print(json);
+    return myPizzas;
+  }
+
+  Future readAndWritePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    appCounter = prefs.getInt('counter') ?? 0;
+    appCounter++;
+
+    await prefs.setInt('counter', appCounter);
+
+    setState(() {
+      appCounter = appCounter;
+    });
+  }
+
+  Future deletePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    setState(() {
+      appCounter = 0;
+    });
+  }
+
+  Future getPaths() async {
+    final docDir = await getApplicationDocumentsDirectory();
+    final tempDir = await getTemporaryDirectory();
+    setState(() {
+      documentsPath = docDir.path;
+      tempPath = tempDir.path;
+    });
+  }
+
+  Future<bool> writeFile() async {
+    try {
+      await myfile.writeAsString('Marsya, Aurelia S, 2341720011');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> readFile() async {
+    try {
+      String fileContent = await myfile.readAsString();
+      setState(() {
+        filetext = fileContent;
+      });
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future writeToSecureStorage() async {
+    await storage.write(key: myKey, value: pwdController.text);
+  }
+
+  Future<String> readFromSecureStorage() async {
+    String secret = await storage.read(key: myKey) ?? '';
+    return secret;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPaths().then((_) {
+      myfile = File('$documentsPath/data.txt');
+      writeFile();
+    });
+  }
+
+  String convertToJSON(List<Pizza> pizzas) {
+    return jsonEncode(pizzas.map((pizza) => jsonEncode(pizza)).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('JSON Marsya Aurelia S', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey,
+
+      ),
+      body: FutureBuilder(
+        future: callPizzas(),
+        builder: (BuildContext context, AsyncSnapshot<List<Pizza>> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          return ListView.builder(
+            itemCount: (snapshot.data == null) ? 0 : snapshot.data!.length,
+            itemBuilder: (BuildContext context, int position) {
+              return ListTile(
+                title: Text(snapshot.data![position].pizzaName),
+                subtitle: Text(
+                  snapshot.data![position].description +
+                      ' - € ' +
+                      snapshot.data![position].price.toString(),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
